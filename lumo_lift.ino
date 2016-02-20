e27a4479947cead23729fb9c225582b1e403d674
yadro <yadrone@gmail.com>
2016-02-20 07:55:40 +0300

    optimization flash memory

--- lumo_lift.ino
+++ lumo_lift.ino
@@ -122,7 +122,27 @@ MPU6050 mpu;
 
 
 #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
+//bool blinkState = false;
+
+
+struct Vector4 {
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+struct Vector3 {
+    float x;
+    float y;
+    float z;
+};
+
+struct VectorInt3 {
+    int32_t x;
+    int32_t y;
+    int32_t z;
+};
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -133,13 +153,16 @@ uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
+int16_t q[4];           // [w, x, y, z]         quaternion container
+
+/*
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+*/
 
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
@@ -160,6 +183,8 @@ bool blink_on = false;
 bool led_on = false;
 unsigned long blink_time = millis();
 unsigned long delta = millis();
+
+
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
@@ -176,8 +201,10 @@ void setup() {
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
+    #ifdef DEBUG
     Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    #endif
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
     // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
@@ -186,22 +213,15 @@ void setup() {
     // crystal solution for the UART timer.
 
     // initialize device
-    Serial.println(F("Initializing I2C devices..."));
+    DEBUG_PRINTLN(F("Initializing I2C devices..."));
     mpu.initialize();
 
     // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    /*Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-    */
+    DEBUG_PRINTLN(F("Testing device connections..."));
+    DEBUG_PRINTLN(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
+    DEBUG_PRINTLN(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
@@ -213,16 +233,16 @@ void setup() {
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
+        DEBUG_PRINTLN(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        DEBUG_PRINTLN(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
         attachInterrupt(0, dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        DEBUG_PRINTLN(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
@@ -232,9 +252,9 @@ void setup() {
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
+        DEBUG_PRINT(F("DMP Initialization failed (code "));
+        DEBUG_PRINT(devStatus);
+        DEBUG_PRINTLN(F(")"));
     }
 
     // configure LED for output
@@ -251,19 +271,6 @@ void loop() {
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
-    // wait for MPU interrupt or extra packet(s) available
-    /*while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-    }*/
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
@@ -275,7 +282,7 @@ void loop() {
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
         mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
+        DEBUG_PRINTLN(F("FIFO overflow!"));
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
@@ -289,125 +296,42 @@ void loop() {
         // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
 
-        #ifdef OUTPUT_TEST
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\tz:");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\ty:");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\tx:");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
 
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
+        
+        mpu.dmpGetQuaternion(q, fifoBuffer);
 
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\tpsi:");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\ttheta:");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\tphi:");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\tz:");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\ty:");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\tx:");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-        #endif
-    
-        #ifdef OUTPUT_TEAPOT
-            // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-        #endif
-
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        VectorFloat v;
+        DEBUG_PRINT(q[0]);
+        DEBUG_PRINT("\t");
+        DEBUG_PRINT(q[1]);
+        DEBUG_PRINT("\t");
+        DEBUG_PRINT(q[2]);
+        DEBUG_PRINT("\t");
+        DEBUG_PRINT(q[3]);
+        DEBUG_PRINTLN("\t");
+        
+        VectorInt3 v;
+        v.x = 0;
+        v.y = 0;
         v.z = 1;
-        applyQuaternion(&v, &q);
-        Serial.print(v.x);
-        Serial.print("\t");
-        Serial.print(v.y);
-        Serial.print("\t");
-        Serial.print(v.z);
-        Serial.println("\t");
+        applyQuaternion(&v, q);
 
-        if (v.z > 0.40 || v.z < 0) {
-          if (millis() - delta > 1000) {
-            blink_on = true;
-          }
+        DEBUG_PRINT(v.x);
+        DEBUG_PRINT("\t");
+        DEBUG_PRINT(v.y);
+        DEBUG_PRINT("\t");
+        DEBUG_PRINT(v.z);
+        DEBUG_PRINTLN("\t");
+
+        if (v.z > 4000 || v.z < 0) {
+            if (millis() - delta > 1000) {
+                blink_on = true;
+            }
         } else {
-          delta = millis();
-          blink_on = false;
+            delta = millis();
+            blink_on = false;
         }
         
         blink();
-                
-        // blink LED to indicate activity
-        //blinkState = !blinkState;
-        //digitalWrite(LED_PIN, blinkState);
     }
 }
 
@@ -434,22 +358,22 @@ void blink() {
   }
 }
 
-void applyQuaternion(VectorFloat *v, Quaternion *q) {
-    float x = v->x;
-    float y = v->y;
-    float z = v->z;
+void applyQuaternion(VectorInt3 *v, int16_t *q) {
+    int32_t x = v->x;
+    int32_t y = v->y;
+    int32_t z = v->z;
 
-    float qx = q->x;
-    float qy = q->y;
-    float qz = q->z;
-    float qw = q->w;
+    int16_t qx = q[1];
+    int16_t qy = q[2];
+    int16_t qz = q[3];
+    int16_t qw = q[0];
 
-    float ix =  qw * x + qy * z - qz * y;
-    float iy =  qw * y + qz * x - qx * z;
-    float iz =  qw * z + qx * y - qy * x;
-    float iw = -qx * x - qy * y - qz * z;
+    int32_t ix =  qw * x + qy * z - qz * y;
+    int32_t iy =  qw * y + qz * x - qx * z;
+    int32_t iz =  qw * z + qx * y - qy * x;
+    int32_t iw = -qx * x - qy * y - qz * z;
 
-    v->x = ix * qw + iw * - qx + iy * - qz - iz * - qy;
-    v->y = iy * qw + iw * - qy + iz * - qx - ix * - qz;
-    v->z = iz * qw + iw * - qz + ix * - qy - iy * - qx;
+    v->x = (ix * qw + iw * - qx + iy * - qz - iz * - qy) / 16384;
+    v->y = (iy * qw + iw * - qy + iz * - qx - ix * - qz) / 16384;
+    v->z = (iz * qw + iw * - qz + ix * - qy - iy * - qx) / 16384;
 }
